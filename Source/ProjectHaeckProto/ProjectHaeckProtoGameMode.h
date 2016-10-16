6657775bdf5299b3c14e02f52588c63a79143aba
Max John Verhoeven <maxverhoeven91@gmail.com>
2016-10-16 15:47:41 +0200

    Initial Commit

---
+++ Source/ProjectHaeckProto/ProjectHaeckProtoGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "ProjectHaeckProtoGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTHAECKPROTO_API AProjectHaeckProtoGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
