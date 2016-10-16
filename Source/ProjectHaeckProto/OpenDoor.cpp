dd31314d10f302aab6c3c5c68a26510f98049160
Max John Verhoeven <maxverhoeven91@gmail.com>
2016-10-16 17:13:56 +0200

    Made a Room

--- Source/ProjectHaeckProto/OpenDoor.cpp
+++ Source/ProjectHaeckProto/OpenDoor.cpp
@@ -1,11 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ProjectHaeckProto.h"
-#include "PositionReport.h"
+#include "OpenDoor.h"
 
 
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+UOpenDoor::UOpenDoor()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -17,19 +17,19 @@ UPositionReport::UPositionReport()
 
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
-	FString objectName = GetOwner()->GetName();
-	FString objectPos =  GetOwner()->GetTransform().GetLocation().ToString();
-	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *objectName, *objectPos);
+	AActor* mahDoor = GetOwner();
+	FRotator doorRotation = FRotator(0, -160.0f, 0);
+	mahDoor->SetActorRotation(doorRotation);
 	// ...
 	
 }
 
 
 // Called every frame
-void UPositionReport::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
 {
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
 
